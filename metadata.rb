fea215aa9b141d4f30fc331c4f9644be874e75a1
Jared Barnett <jmwbarnett@gmail.com>
2016-05-04 13:05:32 -0600

    bumping patch version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'jared.barnett@gmail.com'
 license 'all_rights'
 description 'Used to create a graph of yum repositories'
 long_description 'Chef cookbook for creating a directed acyclic graph of yum repositories. I suppose you *could* create a cycle in your graph, but why would you?'
-version '0.1.1'
+version '0.1.2'
 
 depends 'yum', '~> 3.10.0'
 depends 'httpd', '~> 0.2'
